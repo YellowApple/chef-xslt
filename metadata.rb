686a8e5342013adc6e6c8793b67ff185e13f7543
Ryan S. Northrup <northrup@yellowapple.us>
2014-12-08 01:31:46 -0800

    Fix metadata.rb
    Missing 'name', malformed 'recipe'

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,12 @@
+name             "xslt"
 maintainer       "Phil Cohen"
 maintainer_email "github@phlippers.net"
 license          "MIT"
 description      "Installs libxslt"
 long_description "Please refer to README.md"
-version          "1.0.0"
+version          "1.0.1"
 
-recipe "xslt", "Installs libxslt development packages"
+recipe "default", "Installs libxslt development packages"
 
 %w[centos redhat scientific suse fedora ubuntu debian].each do |os|
   supports os
